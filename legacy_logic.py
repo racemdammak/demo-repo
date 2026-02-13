9a9db0f2513e13b1d8a20f8f307a3a46f6a7e4f9
racem <racemdammak99@gmail.com>
2026-02-13 11:44:57 +0100

    nex project test

---
+++ legacy_logic.py
@@ -0,0 +1,5 @@
+def process_data_old_way(input_val):
+    # This is brittle legacy code
+    # Risk: No null checking
+    result = input_val * 1.5
+    return result
---
+++ modern_app.py
@@ -0,0 +1,5 @@
+from legacy_logic import process_data_old_way
+
+def start_application():
+    # Calling the legacy function
+    print(process_data_old_way(100))
