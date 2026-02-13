c256fa5749e5007318e70b1780f0d1e06a19ed59
racem <racemdammak99@gmail.com>
2026-02-13 20:10:00 +0100

    rer

--- checkout_flow.py
+++ checkout_flow.py
@@ -2,7 +2,4 @@
 from payment_processor import process_transaction
 
 def handle_user_checkout(items, uid):
-    # RECENT CODE
-    # Because of the fix in ingestor.py, the engine will now correctly
-    # see 'process_transaction' as the target, matching the Legacy unit.
     return process_transaction(100, uid)
