206711c6d747b58a4a301dacd97503d66ebaf399
racem <racemdammak99@gmail.com>
2026-02-13 21:20:11 +0100

    req

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from vault import access_secure_data
 
-def request_handler(user_token):
+def request(user_token):
     # This is our RECENT function calling legacy code
     return access_secure_data(user_token)
